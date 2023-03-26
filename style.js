ee6341afcc543f611bd0158ea574ceee01f650a9
rakibulhasan-joy <rakibulhasanjoy009@gmail.com>
2023-03-26 19:56:06 +0600

    menu responsive done

--- style.js
+++ style.js
@@ -1,11 +1,9 @@
  const toggleBtn = document.querySelector(".toggle-btn");
- const toggleBtnIcon = document.querySelector(".toggle-btn i");
+ const toggleBtnIcon = document.querySelector(".toggle-btn .fa-solid");
  const dropdpwnMenu = document.querySelector(".dropdown-menu");
 
-toggleBtn.addEventListener("click", ()=>{
+ toggleBtn.addEventListener("click", ()=>{
     dropdpwnMenu.classList.toggle("open");
     const isOpen = dropdpwnMenu.classList.contains("open");
-
-    toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
 })  
